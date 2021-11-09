3337539e81e51b6f5f2221182aa7ab262be0271d
camila llarrull <camillarrull18@gmail.com>
2021-11-09 15:59:17 -0300

    nana

--- src/components/CardsContainer.vue
+++ src/components/CardsContainer.vue
@@ -3,7 +3,9 @@
     <div class="section-cards">
       <div class="container">
         <h1>soy el card container</h1>
-        <button @click="agregarSeccion()">Crear seccion tarjeta</button>
+        <button class="agregar-card" @click="agregarSeccion()">
+          Crear Tarjeta
+        </button>
         <div class="contenedor-padre-seccion">
           <div
             class="contenedorSeccion"
@@ -12,7 +14,11 @@
           >
             <div style="width: 100%">
               <div class="section-title">
-                <input class="seccionTitulo" type="text" v-model="seccion.titulo">
+                <input
+                  class="seccionTitulo"
+                  type="text"
+                  v-model="seccion.titulo"
+                />
                 <div class="sectionButtons">
                   <button @click="editarTitulo(i)">✏️</button>
                   <button @click="abrirVentana(i)">+</button>
@@ -46,9 +52,7 @@ export default {
       indiceSeleccionado: 0,
       inputTitulo: "",
       inputDescripcion: "",
-      secciones: [
-        
-      ],
+      secciones: [],
     };
   },
   methods: {
@@ -76,23 +80,21 @@ export default {
       this.secciones.push(newSection);
       console.log(this.secciones);
     },
-    editarTitulo() {
-
-    },
+    editarTitulo() {},
   },
 };
 </script>
 
 <style scoped>
 .section-cards {
-  background-color: gray;
+  background-color: #8bcde6;
   min-height: 100vh;
   padding-top: 4rem;
 }
 .contenedorSeccion {
   display: flex;
   padding: 1rem;
-  background: #999;
+  background: #d0d7da;
   border-radius: 5px;
   width: 300px;
 }
@@ -100,14 +102,19 @@ export default {
   display: flex;
   justify-content: space-between;
 }
-.section-title button, .sectionButtons button {
+.section-title button,
+.sectionButtons button {
   padding: 0.5rem;
   border-radius: 5px;
   font-size: 20px;
   border: none;
 }
-.seccionTitulo {
-  
+.agregar-card {
+  padding: 1rem;
+  margin-bottom: 10px;
+  border-radius: 5px;
+  background: #ffffff;
+  border: 1px solid #d0d7da;
 }
 .sectionButtons button:nth-child(1) {
   font-size: 18px;
@@ -147,5 +154,12 @@ export default {
   background-color: white;
   border: 1px solid grey;
   border-radius: 2px;
+}
+.seccionTitulo {
+  background: #d0d7da;
+  border: none;
+  font-size: 20px;
+  font-weight: bold;
+  width: 190px;
 }
 </style>
